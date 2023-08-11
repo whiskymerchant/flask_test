d7e1f44daada49e7e2999a30c55f377110b91733
Anton Plekhov <anton.plekhov@gmail.com>
2023-08-11 17:53:00 +0300

    db, endpoint get_items and POST

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -36,3 +36,39 @@ def get_items():
             }
         output.append(item_data)
     return {'items': output}
+
+
+@app.route('/items/<id>')
+def get_item(id):
+    item = Items.query.get_or_404(id)
+    return {
+        'name': item.name,
+        'description': item.description,
+        'purchase_date': item.purchase_date,
+        'original_cost': item.original_cost,
+        'depricated_cost': item.deprecated_cost,
+        }
+
+
+@app.route('/items', methods=['POST'])
+def add_item():
+    item = Items(
+      name=request.json['name'],
+      description=request.json['description'],
+      purchase_date=request.json['purchase_date'],
+      original_cost=request.json['original_cost'],
+      depreciated_cost=request.json['depreciated_cost'],
+      )
+    db.session.add(item)
+    db.session.commit()
+    return {'id': item.id}
+
+""""
+{
+    "name": "Chair",
+    "description": "Soft wooden chair with armrest painted in oak",
+    "purchase_date": "15.06.2021",
+    "original_cost": "6000",
+    "depricated_cost": "5000"
+}
+"""
